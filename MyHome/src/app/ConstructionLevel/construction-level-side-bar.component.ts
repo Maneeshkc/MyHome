748632c18980ddc1e6932696f570daaf6f50b665
maneesh kc <kc.maneesh@gmail.com>
2026-01-04 21:55:42 +0530

    move left menu to component and populate with construction level

---
+++ MyHome/src/app/ConstructionLevel/construction-level-side-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-construction-level-side-bar',
+  standalone: true,
+  templateUrl: './construction-level-side-bar.component.html',
+  styleUrls: ['./construction-level-side-bar.component.css']
+})
+export class ConstructionLevelSideBarComponent {}
--- MyHome/src/app/app.component.ts
+++ MyHome/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { ConstructionLevelSideBarComponent } from './ConstructionLevel/construction-level-side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ConstructionLevelSideBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
